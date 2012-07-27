06626f363fc208ea7a0a65dc1bbd39a79f0af552
carlohartelucci <charliehartel@yahoo.com>
2012-07-27 19:31:59 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RunSmartProject::Application.config.secret_token = '95fc8d37785b00d6b56f7b569cd1f265e9f233fd10d1e931364d8c06e2fc7c6d7aaff97c0901bf9683cb837fc274f7f39ec6e6070a5721eb1e4d8383f88657b2'
