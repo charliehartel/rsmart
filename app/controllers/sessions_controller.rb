45924a567635f229ef9cc83d8c0ca1d8e135209d
carlohartelucci <charliehartel@yahoo.com>
2012-08-01 17:32:28 -0400

    homepage

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 	  athlete = Athlete.find_by_email(params[:session][:email])
 	  if (athlete && athlete.authenticate(params[:session][:password]))
 	  	sign_in athlete
-	  	redirect_to athlete
+	  	redirect_to schedule_path
 	  else
 
 	  end
